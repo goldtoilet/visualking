12934c6ed428ab73fd2af032bc21853f1053a1a7
goldtoilet <goldtoilet@gmail.com>
2025-12-04 23:45:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import os
 import json
 from json import JSONDecodeError
 st.write("현재 작업 디렉토리:", os.getcwd())
+st.write("현재 디렉토리 파일 목록:", os.listdir(os.getcwd()))
+
 st.write("config.json 존재 여부:", os.path.exists("config.json"))
 
 st.set_page_config(page_title="시각화 마스터", page_icon="📝", layout="centered")
